a8bf7cd56a502900e576d32ec9ca4f719c59afb8
hanan Rebaia <rebaia_hanan@yahoo.fr>
2022-10-04 15:33:10 +0200

    add variable i and j in main file

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-console.log(2 * 4);
-alert("hello");
-
-function sayhello() {
+function sayHello() {
   alert("Bonjour");
 }
+
+let tab = [13, 17, 9, 3];
+console.log(tab);
+
+for (let i = 0; i < tab.length - 1; i++) {
+  console.log("valeur de i:" + i);
+
+  for(let j=0; j < tab.length-1-i,j++) {
+    console.log("j:" + tab[j] + " compare à j+1: " + tab[j + 1]);
+    if (tab[j] > tab[j + 1]) {
+      console.log(
+        "j:" + tab[i] + " et j+1: " + tab[j + 1]  + "à permuter!"
+      );
+      let temp = tab[j + 1];
+      tab[j + 1] = tab[j];
+      tab[j] = temp;
+    }
+  }
+  
+  }
+
+  
+console.log(tab);
