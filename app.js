c3dff370b10e72b3084ddfd2f13e58a3c690c580
hanan Rebaia <rebaia_hanan@yahoo.fr>
2022-10-05 23:10:20 +0200

    add style and test files

---
+++ app.js
@@ -0,0 +1,18 @@
+let maVariable = "un texte à afficher";
+
+console.log(maVariable);
+
+maVariable = "je suis un nouveau texte !";
+
+console.log(maVariable);
+
+let chiffre = 24;
+
+console.log(`il fait ${chiffre} degrès`);
+
+let object = {
+  name: "julie",
+  age: 23,
+  ville: "Lyon",
+};
+console.log(object.ville);
